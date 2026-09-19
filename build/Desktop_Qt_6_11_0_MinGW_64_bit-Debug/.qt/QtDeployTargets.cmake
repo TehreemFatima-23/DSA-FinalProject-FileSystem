48833190a57b5536d76a7a236a3801b50aeb50fd
TehreemFatima-23 <tehreem.twice2005@gmail.com>
2026-09-19 21:02:43 +0500

    Remove build folder and generated build files

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_FileSystemSimulator_FILE D:/FileSystemSimulator/FileSystemSimulator/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/FileSystemSimulator.exe)
-set(__QT_DEPLOY_TARGET_FileSystemSimulator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_FileSystemSimulator_RUNTIME_DLLS D:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
